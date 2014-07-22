5d4458382efbcb38cf8bf821d420c168ebd13b3e
Jon Burman <jonathanburman@timeout.com>
2014-07-22 15:14:46 +0100

    Add style column to products table

--- import.py
+++ import.py
@@ -9,6 +9,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from schema import Base, Product
 
+
+style = raw_input("What is the name of the style? ")
+
+filename = style + '.csv'
+
+
 engine = create_engine('sqlite:///stock.db')
 
 Base.metadata.bind = engine
@@ -19,7 +25,7 @@ session = DBSession()
 
 
 
-file = os.path.join(os.path.dirname(os.path.abspath(__file__)),'top.csv')
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)),filename)
 
 csvfile = open(file,'rb')
 
@@ -32,10 +38,10 @@ flag = False
 for row in reader:
 	if re.match(r'^COR',row[0]) and re.match(r'^EAN',row[2]):
 		flag = True
-		next
+		continue
 	if re.match(r'^$',row[0]):
 		flag = False
 	if flag:
 		print row
-		session.add(Product(description=row[0],size=row[1],ean=row[2]))
+		session.add(Product(style=style.upper(),description=row[0],size=row[1],ean=row[2]))
 		session.commit()
--- schema.py
+++ schema.py
@@ -15,9 +15,10 @@ Base = declarative_base()
 class Product(Base):
 	__tablename__ = 'products'
 	prod_id = Column(Integer, primary_key=True)
+	style = Column(String(40))
 	description = Column( String(100))
 	size = Column(String(40))
-	ean = Column(String(30))
+	ean = Column(String(30), unique=True)
 
 engine = create_engine('sqlite:///stock.db')
 
